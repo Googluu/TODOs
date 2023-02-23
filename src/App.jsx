b19c23145511289a29f01d00592acae1d56f10b5
Sneyder <sneyder.h.rodriguez@gmail.com>
2023-02-23 08:47:31 -0500

    styles para la arquitectura estatica

--- src/App.jsx
+++ src/App.jsx
@@ -20,25 +20,25 @@ function App() {
         </form>
       </header>
       <main className="container mx-auto mt-8 px-4">
-        <div className="rounded-md bg-white px-4">
-          <article>
-            <button className="inline-block h-5 w-5 rounded-full border-2"></button>
-            <p className="text-gray-600">Complete the course</p>
-            <button>
+        <div className="rounded-md bg-white">
+          <article className="flex gap-4 border-b border-b-gray-300 py-4 px-4">
+            <button className="inline-block h-5 w-5 flex-none rounded-full border-2"></button>
+            <p className="grow text-gray-600">Complete the course</p>
+            <button className="flex-none">
               <CrossIcon />
             </button>
           </article>
-          <article>
-            <button className="inline-block h-5 w-5 rounded-full border-2"></button>
-            <p className="text-gray-600">Complete the course</p>
-            <button>
+          <article className="flex gap-4 border-b border-b-gray-300 py-4 px-4">
+            <button className="inline-block h-5 w-5 flex-none rounded-full border-2"></button>
+            <p className="grow text-gray-600">Complete the course</p>
+            <button className="flex-none">
               <CrossIcon />
             </button>
           </article>
-          <article>
-            <button className="inline-block h-5 w-5 rounded-full border-2"></button>
-            <p className="text-gray-600">Complete the course</p>
-            <button>
+          <article className="flex gap-4 border-b border-b-gray-300 py-4 px-4">
+            <button className="inline-block h-5 w-5 flex-none rounded-full border-2"></button>
+            <p className="grow text-gray-600">Complete the course</p>
+            <button className="flex-none">
               <CrossIcon />
             </button>
           </article>
