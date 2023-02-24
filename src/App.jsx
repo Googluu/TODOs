8c54291ef100999f42a9d428a351d5423f3b98e2
Sneyder <sneyder.h.rodriguez@gmail.com>
2023-02-24 14:02:12 -0500

    moon icon send prop

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
             Todo
           </h1>
           <button>
-            <MoonIcon />
+            <MoonIcon fill="#fff" />
           </button>
         </div>
         <form className="mt-8 flex items-center gap-4 overflow-hidden rounded-md bg-white py-4 px-4">
