a8dbfe47f39fbf04a713485b1a4758a955991e9a
Sneyder <sneyder.h.rodriguez@gmail.com>
2023-02-27 08:22:00 -0500

    Modularizacion item

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,8 @@ export * from "./Header";
 export * from "./TodoComputed";
 export * from "./TodoFilter";
 export * from "./TodoForm";
+export * from "./TodoItem";
 export * from "./TodoList";
 export * from "./icons/CrossIcon";
 export * from "./icons/MoonIcon";
+export * from "./index";
