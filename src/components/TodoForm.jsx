102e1c08045321973a04eac0ce320733a7276cb1
Sneyder <sneyder.h.rodriguez@gmail.com>
2023-02-27 10:04:49 -0500

    add todo

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,11 +1,28 @@
-export const TodoForm = () => {
+import { useState } from "react";
+
+export const TodoForm = ({ createTodo }) => {
+  const [title, setTitle] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!title.trim()) return setTitle("");
+
+    createTodo(title);
+    setTitle("");
+  };
   return (
-    <form className="flex items-center gap-4 overflow-hidden rounded-md bg-white py-4 px-4">
+    <form
+      onSubmit={handleSubmit}
+      className="flex items-center gap-4 overflow-hidden rounded-md bg-white py-4 px-4"
+    >
       <span className="inline-block h-5 w-5 rounded-full border-2"></span>
       <input
         type="text"
         placeholder="Create a new todo..."
         className="w-full text-gray-400 outline-none"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
       />
     </form>
   );
