3050d7ebf6e04184b3f0386d39a99b64dfb8e113
Sneyder <sneyder.h.rodriguez@gmail.com>
2023-03-01 11:11:43 -0500

    responsive wailwind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,7 @@ export const Header = () => {
   }, [darkMode]);
 
   return (
-    <header className="container mx-auto px-4">
+    <header className="container mx-auto px-4 md:max-w-xl">
       <div className="flex justify-between">
         <h1 className="mt-5 text-3xl font-semibold uppercase  tracking-[0.4em] text-white">
           Todo
